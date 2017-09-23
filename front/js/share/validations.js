f8e1b1d33cbb9f755ce138fc9cba69d50191e229
judith435 <judithguttmann@gmail.com>
2017-09-24 02:38:38 +0300

    save...

--- front/js/share/validations.js
+++ front/js/share/validations.js
@@ -19,16 +19,15 @@ $(document).ready(function () {
              ctrl: 'movie',
              movie_name: movieName,
              director_id: directorID
-            };
-
+            }; 
             if(app.debugMode){
-              console.log("validations >>>  ajaxData.movie_name  " + ajaxData.movie_name);
-              console.log("validations >>>  ajaxData.director_id " + ajaxData.director_id);
-            }
-
+                console.log("validations >>>  ajaxData.movie_name  " + ajaxData.movie_name);
+                console.log("validations >>>  ajaxData.director_id " + ajaxData.director_id);
+            }  
             $.ajax({
                       type: 'GET',
                       url: app.movieApi,
+                      async: true,
                       data: ajaxData
                   })
                   .done(function(data)
@@ -37,8 +36,9 @@ $(document).ready(function () {
                       //If movie already exists, set response to true -1 means movie was not found
                       response = ( movie.id == -1 ) ? false : true;
                       if(app.debugMode){
-                        console.log(".done  data " + data);
                         console.log(".done  response " + response);
+                        console.log(".done  data " + data);
+                        console.log(".done  movie.id " + movie.id);
                       }
                     })
                   .fail(function(data){
@@ -48,30 +48,65 @@ $(document).ready(function () {
                   return response;
               });
 
-              var validator = $("#frmCU").validate({
-                rules:  {
-                  movie_name: {
-                    required: true,
-                  },
-                  director_id: {
-                    required: true
-                  },
-                  movie_already_exists: {
-                    movie_already_exists: true
-                  },
-                } ,
-                messages: {
-                   movie_name: "No movie name specified",
-                   director: "No director selected",
-                   movie_already_exists: "Movie already exists"
-                },
-                submitHandler: function() {
-                    console.log("submitHandler  response " + response);
-                     generalMovie.ajaxSubmit();
-                  }
-              });
-            
-
               
+    var validator = $("#frmCU").validate({
+      rules:  {
+        movie_name: {
+          required: true 
+        },
+        director_id: {
+          required: true
+        },
+        movie_already_exists: {  movie_already_exists: true
+          // remote: function () {
+          //             var movieName = $('#movieName').val().trim();
+          //             var directorID = $('#DirectorDDL').val().trim();
+          //             if(movieName == "" || directorID == "") {
+          //               return true; //if either movie name of director missing no point in checking
+          //             }
+          //             var ajaxData = {
+          //               ctrl: 'movie',
+          //               async: true,
+          //               movie_name: movieName,
+          //               director_id: directorID
+          //               }; 
+          //               if(app.debugMode){
+          //                   console.log("validations >>>  ajaxData.movie_name  " + ajaxData.movie_name);
+          //                   console.log("validations >>>  ajaxData.director_id " + ajaxData.director_id);
+          //               }  
+          //               $.ajax({
+          //                         type: 'GET',
+          //                         url: app.movieApi,
+          //                         data: ajaxData
+          //                     })
+          //                     .done(function(data)
+          //                       {
+          //                         var movie = JSON.parse(data);
+          //                         //If movie already exists, set response to true -1 means movie was not found
+          //                         response = ( movie.id == -1 ) ? false : true;
+          //                         if(app.debugMode){
+          //                           console.log(".done  response " + response);
+          //                           console.log(".done  data " + data);
+          //                           console.log(".done  movie.id " + movie.id);
+          //                         }
+          //                       })
+          //                     .fail(function(data){
+          //                       console.log(".fail >>>  data  " + data);
+          //                       response = false;
+          //                     })
+          //                     return response;
+          // }
+        },
+      } ,
+      messages: {
+         movie_name: "No movie name specified",
+         director: "No director selected",
+         movie_already_exists: "Movie already exists"
+      },
+      submitHandler: function() {
+           console.log("submitHandler  response " + response);
+           generalMovie.ajaxSubmit();
+        }
+    });
   });
   
