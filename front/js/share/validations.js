789f47885064eefbc01b79c80d0d1ed29c327880
judith435 <judithguttmann@gmail.com>
2017-09-24 03:22:56 +0300

    save....

--- front/js/share/validations.js
+++ front/js/share/validations.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     }
 
     var response;
+    var movieID;
     $.validator.addMethod(
         "movie_already_exists", 
         function() {
@@ -20,10 +21,12 @@ $(document).ready(function () {
              movie_name: movieName,
              director_id: directorID
             }; 
+
             if(app.debugMode){
                 console.log("validations >>>  ajaxData.movie_name  " + ajaxData.movie_name);
                 console.log("validations >>>  ajaxData.director_id " + ajaxData.director_id);
             }  
+
             $.ajax({
                       type: 'GET',
                       url: app.movieApi,
@@ -35,6 +38,7 @@ $(document).ready(function () {
                       var movie = JSON.parse(data);
                       //If movie already exists, set response to true -1 means movie was not found
                       response = ( movie.id == -1 ) ? false : true;
+                      movieID = movie.id; //set for error message
                       if(app.debugMode){
                         console.log(".done  response " + response);
                         console.log(".done  data " + data);
@@ -47,7 +51,6 @@ $(document).ready(function () {
                   })
                   return response;
               });
-
               
     var validator = $("#frmCU").validate({
       rules:  {
@@ -57,51 +60,14 @@ $(document).ready(function () {
         director_id: {
           required: true
         },
-        movie_already_exists: {  movie_already_exists: true
-          // remote: function () {
-          //             var movieName = $('#movieName').val().trim();
-          //             var directorID = $('#DirectorDDL').val().trim();
-          //             if(movieName == "" || directorID == "") {
-          //               return true; //if either movie name of director missing no point in checking
-          //             }
-          //             var ajaxData = {
-          //               ctrl: 'movie',
-          //               async: true,
-          //               movie_name: movieName,
-          //               director_id: directorID
-          //               }; 
-          //               if(app.debugMode){
-          //                   console.log("validations >>>  ajaxData.movie_name  " + ajaxData.movie_name);
-          //                   console.log("validations >>>  ajaxData.director_id " + ajaxData.director_id);
-          //               }  
-          //               $.ajax({
-          //                         type: 'GET',
-          //                         url: app.movieApi,
-          //                         data: ajaxData
-          //                     })
-          //                     .done(function(data)
-          //                       {
-          //                         var movie = JSON.parse(data);
-          //                         //If movie already exists, set response to true -1 means movie was not found
-          //                         response = ( movie.id == -1 ) ? false : true;
-          //                         if(app.debugMode){
-          //                           console.log(".done  response " + response);
-          //                           console.log(".done  data " + data);
-          //                           console.log(".done  movie.id " + movie.id);
-          //                         }
-          //                       })
-          //                     .fail(function(data){
-          //                       console.log(".fail >>>  data  " + data);
-          //                       response = false;
-          //                     })
-          //                     return response;
-          // }
+        duplicate_movie: {  
+            movie_already_exists: true
         },
       } ,
       messages: {
          movie_name: "No movie name specified",
-         director: "No director selected",
-         movie_already_exists: "Movie already exists"
+         director_id: "No director selected",
+         duplicate_movie: "Movie with same name and director already exists"
       },
       submitHandler: function() {
            console.log("submitHandler  response " + response);
