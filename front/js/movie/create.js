89dd0ebeec71e67c6fe75036a0c9363f915fdc73
judith435 <judithguttmann@gmail.com>
2017-09-28 02:10:47 +0300

    save..

--- front/js/movie/create.js
+++ front/js/movie/create.js
@@ -1,8 +1,8 @@
 'use strict'
 
 jQuery(document).ready(function() {
+
     $("#navigation-bar").load("../../templates/navigation-template.html");
     generalMovie.LoadCU_Template(); //load input fields from template
-    generalMovie.LoadDirectors(); //fill directors combo in input fields
-
-
+    generalMovie.LoadDirectors();  //fill directors combo in input fields
+})
